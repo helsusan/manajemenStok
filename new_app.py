f06436d057c8d7fef151f376c8157834803bcd66
HelenaSusan <helenasusan.tanoyo@gmail.com>
2026-02-10 21:41:09 +0700

    update main code

--- new_app.py
+++ new_app.py
@@ -1,9 +1,18 @@
 import streamlit as st
 
 pages = {
-    "Input Data": [
+    "Kelola Data": [
         st.Page("input_data_barang.py", title="Data Barang"),
+        st.Page("input_data_customer.py", title="Data Customer"),
+        st.Page("input_data_supplier.py", title="Data Supplier"),
         st.Page("input_data_penjualan.py", title="Data Penjualan"),
+        st.Page("input_data_pembelian.py", title="Data Pembelian"),
+        st.Page("input_pelunasan_hutang.py", title="Data Hutang"),
+        st.Page("input_pelunasan_piutang.py", title="Data Piutang"),
+    ],
+    "Dashboard": [
+        st.Page("rekapan.py", title="Rekapan Hutang & Piutang"),
+        st.Page("gross_profit.py", title="Gross Profit"),
     ],
 }
 
